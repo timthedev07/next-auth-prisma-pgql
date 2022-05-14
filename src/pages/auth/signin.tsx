4bed3c3d74a6a359df33cb2f0042c0ecaace2274
Tim <harvard90873@gmail.com>
2022-05-14 12:20:17 +0200

    buttons

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,7 +1,42 @@
-import { NextPage } from "next";
+import { NextPage, GetServerSideProps } from "next";
+import { BuiltInProviderType } from "next-auth/providers";
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+} from "next-auth/react";
+import { OAuthButton } from "react-auth-provider-buttons";
 
-const SignIn: NextPage = () => {
-  return <></>;
+interface Props {
+  providers: Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null;
+}
+
+const SignIn: NextPage<Props> = ({ providers }) => {
+  return (
+    <div className="w-screen h-[456px] flex flex-col justify-start items-center pt-36 gap-8">
+      <h2>Sign In</h2>
+      <div className="flex flex-col justify-center items-center min-w-[350px]">
+        {Object.values(providers as any).map((each: any) => (
+          <OAuthButton
+            provider={each.id as any}
+            href={each.callbackUrl}
+            key={each.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      providers: await getProviders(),
+    },
+  };
 };
 
 export default SignIn;

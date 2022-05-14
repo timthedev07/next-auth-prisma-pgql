47e79cb996c1342f607cfaabb9e40c13184535f0
Tim <harvard90873@gmail.com>
2022-05-14 12:07:25 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { SessionProvider } from "next-auth/react";
-import { OAuthButtonProvider } from "react-auth-provider-buttons";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <OAuthButtonProvider>
-      <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
-    </OAuthButtonProvider>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 };
 

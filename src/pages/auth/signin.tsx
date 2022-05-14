bf711b32e684d403153d7153e7238b233b472b65
Tim <harvard90873@gmail.com>
2022-05-14 12:59:42 +0200

    fix

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,7 +4,11 @@ import {
   ClientSafeProvider,
   getProviders,
   LiteralUnion,
+  signIn,
+  useSession,
 } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { OAuthButton } from "react-auth-provider-buttons";
 
 interface Props {
@@ -15,14 +19,23 @@ interface Props {
 }
 
 const SignIn: NextPage<Props> = ({ providers }) => {
+  const { push, isReady, query } = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (isReady && session && session.user) push(query.callbackUrl as string);
+  }, [isReady, session]);
+
   return (
     <div className="w-screen h-[456px] flex flex-col justify-start items-center pt-36 gap-8">
       <h2>Sign In</h2>
       <div className="flex flex-col justify-center items-center min-w-[350px]">
         {Object.values(providers as any).map((each: any) => (
           <OAuthButton
+            onClick={async () => {
+              await signIn(each.id);
+            }}
             provider={each.id as any}
-            href={each.callbackUrl}
             key={each.name}
           />
         ))}

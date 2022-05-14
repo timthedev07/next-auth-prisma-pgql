f871c688de45cf6d6bd5851a656534972ded11c1
Tim <harvard90873@gmail.com>
2022-05-14 11:55:35 +0200

    wrap in context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { SessionProvider } from "next-auth/react";
+import { OAuthButtonProvider } from "react-auth-provider-buttons";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <OAuthButtonProvider>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </OAuthButtonProvider>
   );
 };
 
